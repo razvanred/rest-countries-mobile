a0045a8c2645d5a6a8255a392ac1114cbedaf36a
Răzvan Roșu <razvanred99@gmail.com>
2025-08-05 00:15:08 +0200

    Migrate `:data:model` and `:data:remote` modules to Kotlin Multiplatform

--- data/models/src/main/kotlin/red/razvan/restcountries/data/models/CurrencyId.kt
+++
@@ -1,7 +0,0 @@
-// Copyright 2025 Răzvan Roșu
-// SPDX-License-Identifier: Apache-2.0
-
-package red.razvan.restcountries.data.models
-
-@JvmInline
-value class CurrencyId(val value: String)
---
+++ data/remote/src/commonMain/kotlin/red/razvan/restcountries/data/remote/PlatformModule.kt
@@ -0,0 +1,8 @@
+// Copyright 2025 Răzvan Roșu
+// SPDX-License-Identifier: Apache-2.0
+
+package red.razvan.restcountries.data.remote
+
+import org.koin.core.module.Module
+
+internal expect val PlatformModule: Module
