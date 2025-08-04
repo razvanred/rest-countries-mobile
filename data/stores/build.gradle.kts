1ce665726f9e8cbf0bb01c03ec43b858e741e06f
Răzvan Roșu <razvanred99@gmail.com>
2025-08-05 00:24:04 +0200

    Migrate `:data:stores` module to Kotlin Multiplatform

--- data/stores/build.gradle.kts
+++ data/stores/build.gradle.kts
@@ -2,22 +2,38 @@
 // SPDX-License-Identifier: Apache-2.0
 
 plugins {
-  id("red.razvan.restcountries.kotlin.android")
+  id("red.razvan.restcountries.kotlin.multiplatform")
   id("red.razvan.restcountries.android.library")
+}
+
+kotlin {
+  androidTarget()
+  iosArm64()
+  iosSimulatorArm64()
+
+  sourceSets {
+    val commonMain by getting {
+      dependencies {
+        api(projects.data.models)
+        api(libs.koin.core)
+        api(libs.store)
+
+        implementation(projects.data.db)
+        implementation(projects.data.remote)
+        implementation(projects.core.utils)
+        implementation(projects.commons.kotlinxCoroutines)
+        implementation(projects.core.kotlinxCoroutines)
+      }
+    }
+
+    val androidMain by getting {
+      dependencies {
+        api(libs.koin.android)
+      }
+    }
+  }
 }
 
 android {
   namespace = "red.razvan.restcountries.data.stores"
-}
-
-dependencies {
-  api(projects.data.models)
-  api(libs.koin.android)
-  api(libs.store)
-
-  implementation(projects.data.db)
-  implementation(projects.data.remote)
-  implementation(projects.core.utils)
-  implementation(projects.commons.kotlinxCoroutines)
-  implementation(projects.core.kotlinxCoroutines)
 }
